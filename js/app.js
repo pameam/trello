19f274f84f8c0594906b3c9bdb903f49d1b66d63
pameam <pamela.anayhuaman@gmail.com>
2017-11-20 00:37:52 -0500

    añadiendo nuevos estilos y funcionalidad a la seccion añadir lista

--- js/app.js
+++ js/app.js
@@ -2,11 +2,62 @@
 //
 // Se mostrará el texto "Añadir una lista ...".
 // Al hacer click se debe ocultar el texto y mostrar un formulario.
-// El formulario está conformado por un input y un botón para que pueda añadir tareas a tu lista.
+window.addEventListener('load', function() {
+  var container = document.body.children[1].children[0].children[1];
+  var button = document.body.children[1].children[0].children[1].children[0];
+  // var form = document.body.children[1].children[0].children[1].children[0];
+  button.addEventListener('click', function() {
+    // button.setAttribute('id', 'hidden');
+    container.classList.add('hidden');
+    button.classList.add('hidden');
+    var form = document.body.children[1].children[0].children[2].children[0];
+    form.classList.add('show');
+    // form.setAttribute('id', 'hidden');
+    // El formulario está conformado por un input y un botón para que pueda añadir tareas a tu lista.
+    var input = document.createElement('input');
+    var text = document.createTextNode(text);
+    var inputButton = document.createElement('button');
+    input.appendChild(text);
+    // document.body.children[1].children[0].children[2].appendChild(input);
+    document.body.children[1].children[0].children[2].children[0].appendChild(input);
+    input.setAttribute('placeholder', 'añadir lista');
+    input.setAttribute('id', 'inputList');
+    // document.body.children[1].children[0].children[2].appendChild(inputButton);
+    document.body.children[1].children[0].children[2].children[0].appendChild(inputButton);
+    inputButton.innerHTML = 'Guardar';
+    inputButton.setAttribute('id', 'append');
+    // var content = document.body.children[1].children[0].children[2].children[0].children[0].value;
+    // var save = document.getElementById('append');
+    // save.addEventListener('click', function() {
+    //   var input2 = document.createElement('p');
+    //   var text2 = document.createTextNode(content);
+    //   input2.appendChild(text2);
+    //   document.body.children[1].children[0].children[2].appendChild(input2);
+    // });
+  });
+});
+
+
+var text = document.getElementById('inputList');
+text.addEventListener('input', function() {
+  var input2 = document.createElement('p');
+  var text2 = document.createTextNode(text);
+  input2.appendChild(text2);
+  document.body.children[1].children[0].children[2].appendChild(input2);
+});
+
 // Versión 0.0.2
-//
 // Al dar click en el botón de "Guardar", se mostrará un nuevo cuadro donde estará el nombre de la lista agregada.
 // Mostrar un texto de "Añadir una tarea" dentro de la lista.
+// var list = document.getElementById('inputList').value;
+// console.log(input.getAttribute('value'));
+// var save = document.getElementById('append');
+// save.addEventListener('click', function() {
+//   var input2 = document.createElement('input');
+//   var text2 = document.createTextNode(list);
+//   input2.appendChild(text2);
+//   document.body.children[1].children[0].children[2].children[0].appendChild(input2);
+// });
 // Versión 0.0.3
 //
 // Al dar click en "Añadir una tarea", deberá mostrar un formulario con un textarea y un botón que diga "Añadir".
